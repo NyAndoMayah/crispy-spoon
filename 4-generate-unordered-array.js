9ed94053c51089ce7603039f9673ac55b5513e15
NyAndoMayah <99728289+NyAndoMayah@users.noreply.github.com>
2022-04-01 17:03:08 +0300

    feat:changes in 3

--- 4-generate-unordered-array.js
+++ 4-generate-unordered-array.js
@@ -1,15 +1,20 @@
 const { arrayContain } = require('./3-array-contain');
 
 function generateUnorderedArray(size) {
-  let array = [];
-  for (let i = 0; i < size; i++) {
-    let number = Math.floor(Math.random() * 100);
-    while (arrayContain(array, number)) {
-      number = Math.floor(Math.random() * 100);
+  let array = [];//1
+  for (let i = 0; i < size; i++) {//7
+    let number = Math.floor(Math.random() * 100);//4
+    while (arrayContain(array, number)) //14n+1+1
+      number = Math.floor(Math.random() * 100);//4
     }
-    array.push(number);
+    array.push(number);//3
   }
-  return array;
+  return array;//1
 }
 
 module.exports = { generateUnorderedArray };
+/*
+T4 = 1+n(4+14n+1+1+4+3+1)= 1+n(4+14n)=14n**2+4n+1
+quand n tend vers + infini alors T4= +infini
+donc la complexité est O(n**2)
+*/
