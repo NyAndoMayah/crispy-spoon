5b6e1b4bac56c58b185832430c97da384817d87e
NyAndoMayah <99728289+NyAndoMayah@users.noreply.github.com>
2022-04-01 17:10:48 +0300

    feat changes in 6

--- 6-main.js
+++ 6-main.js
@@ -12,10 +12,10 @@ function sort(array) {
   return merge(sort(left), sort(right));//46n+nlog(n)+nlog(n)
 }
 
-const array = generateUnorderedArray(10);//14n**2+4n+1+1
+const array = generateUnorderedArray(10);//14n**2+21n+1+1
 console.log(array);//1
 console.log(sort(array));//nlog(n)
 /*
-T6=3+1+5+1+n +2+1+n+4+46n+nlog(n)+nlog(n)+14n**2+4n+1+1+1+nlog(n)
+T6=3+1+5+1+n+2+1+n+4+46n+nlog(n)+nlog(n)+14n**2+21n+1+1+1+nlog(n)
 quand n tend vers +infini alors T6 
 */
