2a0b358b8fe9d5edeb4e58244689cf7db0592cdf
NyAndoMayah <99728289+NyAndoMayah@users.noreply.github.com>
2022-04-01 17:02:30 +0300

    changes in 2

--- 2-merge.js
+++ 2-merge.js
@@ -1,25 +1,29 @@
 function merge(array1, array2) {
-  let mergedArray = [];
-  let i = 0;
-  let j = 0;
-  while (i < array1.length && j < array2.length) {
-    if (array1[i] < array2[j]) {
-      mergedArray.push(array1[i]);
-      i++;
+  let mergedArray = [];//1
+  let i = 0;//1
+  let j = 0;//1
+  while (i < array1.length && j < array2.length) {//9
+    if (array1[i] < array2[j]) {//5
+      mergedArray.push(array1[i]);// 4
+      i++;//2
     } else {
-      mergedArray.push(array2[j]);
-      j++;
+      mergedArray.push(array2[j]);//4
+      j++;//2
     }
   }
-  while (i < array1.length) {
-    mergedArray.push(array1[i]);
-    i++;
+  while (i < array1.length) {//4
+    mergedArray.push(array1[i]);//4
+    i++;//2
   }
-  while (j < array2.length) {
-    mergedArray.push(array2[j]);
-    j++;
+  while (j < array2.length) {//4
+    mergedArray.push(array2[j]);//4
+    j++;//2
   }
-  return mergedArray;
+  return mergedArray;//1
 }
 
 module.exports = { merge };
+/*
+T2 = n(9+5+4+2+4+2) +n(4+4+2)+n(4+4+2)+1=26n + 10n +10n= 46n
+si n tend vers + infini alors T2=+infini donc la complexité est O(n).
+*/
