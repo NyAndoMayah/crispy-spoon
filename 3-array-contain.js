046061137c7dee22fba07644c76576203767dd42
NyAndoMayah <99728289+NyAndoMayah@users.noreply.github.com>
2022-04-01 17:02:50 +0300

    feat:changes in 3

--- 3-array-contain.js
+++ 3-array-contain.js
@@ -1,10 +1,14 @@
 function arrayContain(array, number) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === number) {
-      return true;
+  for (let i = 0; i < array.length; i++) {//8
+    if (array[i] === number) {//4
+      return true;//1 
     }
   }
-  return false;
+  return false;//1
 }
 
 module.exports = { arrayContain };
+/*
+T3 = n(8+4+1+1)= 14n
+si n tend vers +infini alors T3= infini, la complexité est donc O(n)
+*/
