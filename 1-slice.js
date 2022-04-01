1683bb80e47387cd8fd74f3886f4f69119a7d9b0
NyAndoMayah <99728289+NyAndoMayah@users.noreply.github.com>
2022-04-01 17:02:13 +0300

    feat: changes in 1-slice

--- 1-slice.js
+++ 1-slice.js
@@ -1,9 +1,14 @@
 function slice(array, start, end) {
-  let slicedArray = [];
-  for (let i = start; i < end; i++) {
-    slicedArray.push(array[i]);
+  let slicedArray = [];//1
+  for (let i = start; i < end; i++) {//8
+    slicedArray.push(array[i]);//4
   }
-  return slicedArray;
+  return slicedArray;//1
 }
 
 module.exports = { slice };
+/*
+T1 =  1 + n (8+4+1)=1+13n
+or pour n tend vers + infini, T1=+infini 
+donc la complexité est O(n)
+*/
